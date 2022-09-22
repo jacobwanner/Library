f7db013dfa05e7c9a9791bd803621dcaeee39c99
Jacob Wanner <106052324+jacobwanner@users.noreply.github.com>
2022-09-21 23:23:59 -0400

    books now can be added to library

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+let myLibrary = [];
+
 function book(title, author, pages, read) {
     this.title = title
     this.author = author
@@ -9,7 +11,12 @@ function book(title, author, pages, read) {
 }
 
 const theHobbit = new book('The Hobbit', 'J.R.R. Tolkien', '295 pages', 'read')
+const theLOTR1 = new book("The Fellowship of the Ring (The Lord of the Rings, Book 1)", "J.R.R. Tolkien", "549", "read")
+const theLOTR2 = new book("The Two Towers (The Lord of the Rings, Book 2)", "J.R.R. Tolkien", "485", "read")
+const theLOTR3 = new book("The Return of the King (The Lord of the Rings, Book 3)", "J.R.R. Tolkien", "578", "read")
 
-function addNewBook(){
-    
+function addBookToLibrary(){
+    myLibrary.push(theHobbit, theLOTR1, theLOTR2, theLOTR3)
 }
+addBookToLibrary()
+console.log(myLibrary)
